cabd044aa771bb2b99169843e5326b3448d21c17
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2022-04-06 09:17:14 +0100

    ,
    n

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Salut from "./components/Salut"
 
 function App() {
   return (
     <div className="App">
-     
+      <Salut/>
     </div>
   );
 }
